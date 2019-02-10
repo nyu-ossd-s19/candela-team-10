45b88612bc1689ad559d0ecddf55d1fa4a4fffd8
tojimjiang <tojimjiang@gmail.com>
2019-02-10 18:55:04 -0500

    added event listener for extension

--- orange.js
+++ orange.js
@@ -1,19 +1,16 @@
-// Immidately Invoked JS
-(function () {
-    function loadJquery(callback) {
-        var script = document.createElement("script")
-        script.type = "text/javascript";
-        script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js";
-        document.getElementsByTagName("head")[0].appendChild(script);
 
-        script.onload = function () {
-            console.log('jquery loaded');
-            callback();
-        };
-    }
-    
-    loadJquery(append);
-})();
+function loadJquery(callback) {
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    script.onload = function () {
+        console.log('jquery loaded');
+        callback();
+    };
+}
+
 
 // Append HTML and CSS via Jquery for element wide filtering
 function append() {
@@ -33,11 +30,11 @@ function append() {
 ';
     document.body.appendChild(wrapper);
     console.log("appended");
-        $("html").css("filter","url(#orange)")
-        $("html").css("backdrop-filter","url(#orange)")
-        console.log("js added");
-        backgroundShift();
-        console.log("background shifted");
+    $("html").css("filter", "url(#orange)")
+    $("html").css("backdrop-filter", "url(#orange)")
+    console.log("js added");
+    backgroundShift();
+    console.log("background shifted");
 }
 
 // Shift background using jquery and css and some math.
@@ -61,24 +58,24 @@ function backgroundShift() {
     let array = color.split(",");
     console.log(array);
     console.log($('html').color);
-    
+
     // For cases when background was not set
     if (unset) {
-        for(let i = 0; i < 3; i++ ) {
+        for (let i = 0; i < 3; i++) {
             if (array[i] < 1) {
                 array[i] = 255;
             }
         }
     }
     console.log(array);
-    array[1] = Math.floor(array[1]*0.72);
-    array[2] = Math.floor(array[2]*0.23);
+    array[1] = Math.floor(array[1] * 0.72);
+    array[2] = Math.floor(array[2] * 0.23);
     console.log(array);
 
     // Convert values back to hex values
     // to pass back into CSS
     let hexString = "#";
-    for(let i = 0; i < 3; i++ ) {
+    for (let i = 0; i < 3; i++) {
         hexPart = array[i].toString(16)
         if (hexPart.length % 2) {
             hexPart = '0' + hexPart;
@@ -88,3 +85,10 @@ function backgroundShift() {
     console.log(hexString);
     $('html').css("background-color", hexString);
 }
+
+
+function load() {
+    loadJquery(append);
+}
+
+browser.browserAction.onClicked.addListener(load);
